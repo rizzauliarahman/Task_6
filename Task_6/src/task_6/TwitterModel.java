622dd4da937109412701a39b4f7f61cf81f0cce4
ARCHATACOMP <mrestu56@yahoo.com>
2016-04-13 21:15:46 +0700

    Modifikasi Task_6

---
+++ Task_6/src/task_6/TwitterModel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task_6;
+
+import twitter4j.Twitter;
+
+/**
+ *
+ * @author PRAKTIKUM
+ */
+public class TwitterModel {
+    Twitter twitter;
+    
+    
+}
